f348d08bae04a6813282e44fa2dec8eeb7e55913
kellims <kellisimon18@gmail.com>
2023-05-20 11:37:46 -0600

    Locations Page

--- src/pages/Locations.js
+++ src/pages/Locations.js
@@ -1,5 +1,58 @@
-function Locations(props) {
-    return <h1>Here's where you'll see my list of locations</h1>;
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+
+
+
+
+
+function Locations() {
+    const [locations, setLocations] = useState([]);
+    const [locationsForm, setLocationsForm] = useState({
+        place: "",
+        date: ""
+    })
+
+    async function getLocations() {
+        try {
+            
+            let mylocations = await fetch('http://localhost:4000/locations')
+            mylocations = await mylocations.json();
+            setLocations(mylocations);
+        } catch(err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        getLocations();
+    }, []);
+
+    console.log(locations);
+
+    function loaded(arr) {
+        return(
+            <>
+                {arr.map((locations, idx) => {
+                    return(
+                        <div key={idx}>
+                            <Link to={`/locations/${locations._id}`}>
+                                <h2>Location: {locations.place}</h2>
+                            </Link>
+                            <h3>Date: {locations.date}</h3>
+            
+                            <hr />
+                        </div>
+                    )
+                })}
+            </>
+        )
+    }
+
   }
+
+
   
   export default Locations;
+
+
