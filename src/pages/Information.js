18748c2e1ee50848a082c7b52146188198bb0612
kellims <kellisimon18@gmail.com>
2023-05-20 09:00:02 -0600

    pages added

---
+++ src/pages/Information.js
@@ -0,0 +1,5 @@
+function Information(props) {
+    return <h1>Here's where you'll see all the information about each Location</h1>;
+  }
+  
+  export default Information;
---
+++ src/pages/Locations.js
@@ -0,0 +1,5 @@
+function Locations(props) {
+    return <h1>Here's where you'll see my list of locations</h1>;
+  }
+  
+  export default Locations;
