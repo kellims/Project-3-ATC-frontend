4d9b0289e60ff4cffd5213e18084aa3a499f44ae
kellims <kellisimon18@gmail.com>
2023-05-20 08:47:07 -0600

    frontend

---
+++ src/components/Footer.js
@@ -0,0 +1,5 @@
+function Footer(props) {
+    return <h1>Here's my Footer</h1>;
+  }
+  
+  export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+function Header(props) {
+    return <h1>Here's my Header</h1>;
+  }
+  
+  export default Header;
