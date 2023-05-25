8d1ac2c8a4db658777dfc26ebb4830b044934724
kellims <kellisimon18@gmail.com>
2023-05-24 18:18:34 -0600

    moved over Edit code

--- src/pages/LocationEdit.js
+++ src/pages/LocationEdit.js
@@ -4,7 +4,29 @@ import { useNavigate } from "react-router-dom";
 
 
 function LocationEdit() {
-    
+    const [editForm, setEditForm] = useState(locationInfo)
+
+  const updateLocation = async (e) => {
+    e.preventDefault()
+
+    try {
+      await fetch(URL, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(editForm),
+      })
+
+      getLocation()
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+const handleChange = event => {
+      setEditForm({ ...editForm, [event.target.name]: event.target.value })
+}
 }
 
 export default LocationEdit;
